26e1ba1e4e3ed0fe3e17054bbe08401c01fd8054
wuz1mu <kingaal93@gmail.com>
2023-11-23 23:28:46 +0700

    enable health

--- backend/inventory-service/src/app.module.ts
+++ backend/inventory-service/src/app.module.ts
@@ -3,9 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecordingModule } from './recording/recording.module';
 import { LectureModule } from './lecture/lecture.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/configuration';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [RecordingModule, LectureModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+    HealthModule,
+    RecordingModule, 
+    LectureModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
