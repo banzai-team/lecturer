28e16728e774a14e5bae4c24f40605518c470ef1
wuz1mu <kingaal93@gmail.com>
2023-11-24 02:39:40 +0700

    create separate file module

--- backend/inventory-service/src/config/configuration.ts
+++ backend/inventory-service/src/config/configuration.ts
@@ -1,3 +1,6 @@
+import { DataSourceOptions } from "typeorm";
+import dbConfig from "./db.config";
+
 const bullMqConfig = () => ({
     host: process.env.REDIS_HOST || 'localhost',
     port: parseInt(process.env.REDIS_PORT) || 6379, 
@@ -5,7 +8,8 @@ const bullMqConfig = () => ({
 
 export default () => ({
     port: parseInt(process.env.PORT) || 3000,
-    bullMq: bullMqConfig()
+    bullMq: bullMqConfig(),
+    db: dbConfig()
 })
 
 export const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
