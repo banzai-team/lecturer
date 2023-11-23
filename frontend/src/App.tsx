6f43f248d3772d3e60fe0314f3f9a39ce42e2fd7
Artem Astapenko <jamakase54@gmail.com>
2023-11-23 21:00:46 +0300

    Fix counter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,13 @@
-import {useState} from 'react'
 import './App.css'
 
-import {Router} from "./pages/router";
-import {HelmetProvider} from "react-helmet-async";
+import { Router } from "./pages/router";
+import { HelmetProvider } from "react-helmet-async";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-      <HelmetProvider>
-          <Router/>
-      </HelmetProvider>
+    <HelmetProvider>
+      <Router/>
+    </HelmetProvider>
   )
 }
 
