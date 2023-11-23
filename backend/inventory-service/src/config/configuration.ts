ef37701056304aa7c73c99db80ea94bcd2ca5c3b
wuz1mu <kingaal93@gmail.com>
2023-11-24 00:46:10 +0700

    docker compose upd

--- backend/inventory-service/src/config/configuration.ts
+++ backend/inventory-service/src/config/configuration.ts
@@ -1,6 +1,6 @@
 const bullMqConfig = () => ({
-    host: process.env.BULL_MQ_HOST || 'localhost',
-    port: parseInt(process.env.BULL_MQ_PORT) || 6379, 
+    host: process.env.REDIS_HOST || 'localhost',
+    port: parseInt(process.env.REDIS_PORT) || 6379, 
 })
 
 export default () => ({
