fdc2a0f494eb360e889bd0a56a320b1f3d5d775b
wuz1mu <kingaal93@gmail.com>
2023-11-23 23:16:49 +0700

    add bullmq dependency and config

--- backend/inventory-service/src/config/configuration.ts
+++ backend/inventory-service/src/config/configuration.ts
@@ -1,5 +1,11 @@
+const bullMqConfig = () => ({
+    host: process.env.BULL_MQ_HOST || 'localhost',
+    port: parseInt(process.env.BULL_MQ_PORT) || 6379, 
+})
+
 export default () => ({
     port: parseInt(process.env.PORT) || 3000,
-});
+    bullMq: bullMqConfig()
+})
 
 export const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
