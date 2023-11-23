d71db8aed607301f3427bc4504195ade8825308d
wuz1mu <kingaal93@gmail.com>
2023-11-23 23:29:02 +0700

    register bullmq

--- backend/inventory-service/src/app.module.ts
+++ backend/inventory-service/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecordingModule } from './recording/recording.module';
 import { LectureModule } from './lecture/lecture.module';
+import { BullModule } from '@nestjs/bullmq';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from './config/configuration';
 import { HealthModule } from './health/health.module';
@@ -12,6 +13,10 @@ import { HealthModule } from './health/health.module';
     ConfigModule.forRoot({
       load: [configuration],
       isGlobal: true,
+    }),
+    BullModule.forRootAsync({
+      useFactory: (configService: ConfigService) => configService.get('bullMq'),
+      inject: [ConfigService],
     }),
     HealthModule,
     RecordingModule, 
