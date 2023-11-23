879ab1a909840794ab06ffc819d8af3666c67f52
Artem Astapenko <jamakase54@gmail.com>
2023-11-23 19:18:56 +0300

    Add initial apps

---
+++ infra/main.tf
@@ -0,0 +1,62 @@
+locals {
+  zone = "ru-central1-a"
+}
+
+module "yc-vpc" {
+  source              = "git@github.com:terraform-yc-modules/terraform-yc-vpc.git"
+  network_name        = "lecturer-network"
+  network_description = "lecturer vpc"
+  private_subnets     = [
+    {
+      name           = "subnet-1"
+      zone           = local.zone
+      v4_cidr_blocks = ["10.10.0.0/24"]
+    }
+  ]
+}
+
+module "kube" {
+  source     = "git@github.com:terraform-yc-modules/terraform-yc-kubernetes.git"
+  network_id = "${module.yc-vpc.vpc_id}"
+
+  public_access = true
+
+  master_locations = [
+    for s in module.yc-vpc.private_subnets :
+    {
+      zone      = s.zone,
+      subnet_id = s.subnet_id
+    }
+  ]
+
+  master_maintenance_windows = [
+    {
+      day        = "monday"
+      start_time = "23:00"
+      duration   = "3h"
+    }
+  ]
+
+  master_logging = {
+    enabled = false
+    folder_id              = null
+    enabled_kube_apiserver = true
+    enabled_autoscaler     = true
+    enabled_events         = true
+  }
+
+  node_groups = {
+    "yc-k8s-ng-01" = {
+      description = "Kubernetes nodes group 01"
+      auto_scale = {
+        initial = 1
+        min = 1
+        max = 2
+      }
+      node_labels = {
+        role        = "worker-01"
+        environment = "app"
+      }
+    },
+  }
+}
