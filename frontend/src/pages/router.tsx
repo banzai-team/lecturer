7f65bc80273aa91de7e62618256b0031e311941f
chOlya <Chvyrova.o@mail.ru>
2023-11-24 03:31:39 +0300

    add upload page

--- frontend/src/pages/router.tsx
+++ frontend/src/pages/router.tsx
@@ -7,11 +7,12 @@ import IndexPage from "./IndexPage"
 import MainLayout from "../components/MainLayout";
 import EmptyPage from "./EmptyPage";
 import LecturePage from "./LecturePage";
-
+import UploadPage from "./UploadPage";
 
 export const  Routes = {
     ROOT: "/",
     LECTURE: "/lecture",
+    UPLOAD: "/upload",
 };
 
 const Loading = () => <EmptyPage><CircularProgress color="primary"/></EmptyPage>;
@@ -29,7 +30,12 @@ export const InnerRouter: React.FC = () => {
                 {
                     path: `${Routes.LECTURE}/:id`,
                     element: <LecturePage/>,
-                }, {
+                },
+                {
+                    path: Routes.UPLOAD,
+                    element: <UploadPage />,
+                },
+                {
                     path: '*',
                     element: <EmptyPage text="ERROR 404"/>,
                 },
